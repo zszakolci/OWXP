329db1f354d4abce96f5330a77f0e4f0a1aa75c9
Tamas Molnar <tamas.molnar@liferay.com>
2016-05-27 14:42:50 +0200

    gw formatSource

--- src/main/java/com/liferay/owxp/service/OWXPJournalArticleLocalServiceWrapper.java
+++ src/main/java/com/liferay/owxp/service/OWXPJournalArticleLocalServiceWrapper.java
@@ -1,21 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.owxp.service;
 
 import com.liferay.journal.service.JournalArticleLocalServiceWrapper;
-
 import com.liferay.portal.kernel.service.ServiceWrapper;
 
 import org.osgi.service.component.annotations.Component;
 
-@Component(
-	immediate = true,
-	property = {
-	},
-	service = ServiceWrapper.class
-)
-public class OWXPJournalArticleLocalServiceWrapper extends JournalArticleLocalServiceWrapper {
+/**
+ * @author Tamas Molnar
+ */
+@Component(immediate = true, property = {}, service = ServiceWrapper.class)
+public class OWXPJournalArticleLocalServiceWrapper
+	extends JournalArticleLocalServiceWrapper {
 
-        public OWXPJournalArticleLocalServiceWrapper() {
-            super(null);
-        }
+	public OWXPJournalArticleLocalServiceWrapper() {
+		super(null);
+	}
 
 }
