9597856ae05e06f961edfcadc983259ae700e6db
Tamas Molnar <tamas.molnar@liferay.com>
2016-05-30 16:11:14 +0200

    gw formatSource

--- src/main/java/com/liferay/owxp/subscribe/portlet/OWXPSubscribePortlet.java
+++ src/main/java/com/liferay/owxp/subscribe/portlet/OWXPSubscribePortlet.java
@@ -1,24 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.owxp.subscribe.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 import javax.portlet.Portlet;
 
 import org.osgi.service.component.annotations.Component;
-
-import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-
 @Component(
 	immediate = true,
 	property = {
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.instanceable=true",
 		"javax.portlet.display-name=owxp-subscribe Portlet",
-		"javax.portlet.security-role-ref=power-user,user",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.resource-bundle=content.Language"
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
 	},
 	service = Portlet.class
 )
+/**
+ * @author Tamas Molnar
+ */
 public class OWXPSubscribePortlet extends MVCPortlet {
-
 }
