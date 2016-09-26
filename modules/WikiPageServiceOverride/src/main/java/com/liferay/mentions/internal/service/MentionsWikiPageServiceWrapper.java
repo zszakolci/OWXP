119d61e52ae97bfe3ecea70ea9565d10ebac6ab2
Tamas Molnar <tamas.molnar@liferay.com>
2016-09-26 12:32:01 +0200

    OWXP-7 Add mentions to Wiki pages (copy-paste from outdated OWXP branch)

--- modules/WikiPageServiceOverride/src/main/java/com/liferay/mentions/internal/service/MentionsWikiPageServiceWrapper.java
+++ modules/WikiPageServiceOverride/src/main/java/com/liferay/mentions/internal/service/MentionsWikiPageServiceWrapper.java
@@ -1,21 +1,123 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.mentions.internal.service;
 
+import com.liferay.mentions.configuration.MentionsGroupServiceConfiguration;
+import com.liferay.mentions.util.MentionsNotifier;
+import com.liferay.mentions.util.MentionsUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.wiki.model.WikiPage;
+import com.liferay.wiki.service.WikiPageLocalService;
 import com.liferay.wiki.service.WikiPageLocalServiceWrapper;
 
-import com.liferay.portal.kernel.service.ServiceWrapper;
+import java.io.Serializable;
+
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
-@Component(
-	immediate = true,
-	property = {
-	},
-	service = ServiceWrapper.class
-)
-public class MentionsWikiPageServiceWrapper extends WikiPageLocalServiceWrapper {
+/**
+ * @author Norbert Kocsis
+ */
+@Component(immediate = true, service = ServiceWrapper.class)
+public class MentionsWikiPageServiceWrapper
+	extends WikiPageLocalServiceWrapper {
 
 	public MentionsWikiPageServiceWrapper() {
 		super(null);
 	}
+
+	public MentionsWikiPageServiceWrapper(
+		WikiPageLocalService wikiPageLocalService) {
+
+		super(wikiPageLocalService);
+	}
+
+	@Override
+	public WikiPage updateStatus(
+			long userId, WikiPage page, int status,
+			ServiceContext serviceContext,
+			Map<String, Serializable> workflowContext)
+		throws PortalException {
+
+		int oldStatus = page.getStatus();
+
+		page = super.updateStatus(
+			userId, page, status, serviceContext, workflowContext);
+
+		if ((status != WorkflowConstants.STATUS_APPROVED) ||
+			(oldStatus == WorkflowConstants.STATUS_IN_TRASH)) {
+
+			return page;
+		}
+
+		long siteGroupId = PortalUtil.getSiteGroupId(page.getGroupId());
+
+		if (!MentionsUtil.isMentionsEnabled(siteGroupId)) {
+			return page;
+		}
+
+		String contentURL = (String)serviceContext.getAttribute("contentURL");
+
+		if (Validator.isNull(contentURL)) {
+			serviceContext.setAttribute(
+				"contentURL", workflowContext.get("url"));
+		}
+
+		MentionsGroupServiceConfiguration mentionsGroupServiceConfiguration =
+			_configurationProvider.getCompanyConfiguration(
+				MentionsGroupServiceConfiguration.class, page.getCompanyId());
+
+		_mentionsNotifier.notify(
+			page.getUserId(), page.getGroupId(), page.getTitle(),
+			page.getContent(), WikiPage.class.getName(), page.getPageId(),
+			mentionsGroupServiceConfiguration.assetEntryMentionEmailSubject(),
+			mentionsGroupServiceConfiguration.assetEntryMentionEmailBody(),
+			serviceContext);
+
+		return page;
+	}
+
+	@Reference(unbind = "-")
+	protected void setConfigurationProvider(
+		ConfigurationProvider configurationProvider) {
+
+		_configurationProvider = configurationProvider;
+	}
+
+	@Reference(unbind = "-")
+	protected void setMentionsNotifier(MentionsNotifier mentionsNotifier) {
+		_mentionsNotifier = mentionsNotifier;
+	}
+
+	@Reference(unbind = "-")
+	protected void setWikiPageLocalService(
+		WikiPageLocalService wikiPageLocalService) {
+
+		_wikiPageLocalService = wikiPageLocalService;
+	}
+
+	private ConfigurationProvider _configurationProvider;
+	private MentionsNotifier _mentionsNotifier;
+	private WikiPageLocalService _wikiPageLocalService;
 
 }
