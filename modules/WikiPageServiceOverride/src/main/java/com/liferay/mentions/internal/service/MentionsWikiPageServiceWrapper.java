4a950540e12bc44b4bd287afc77f82320bcb2ebb
Tamas Molnar <tamas.molnar@liferay.com>
2016-10-04 14:00:57 +0200

    OWXP-13 Copy notifySubscribers method and it's dependencies from WikiPageLocalServiceImpl

--- modules/WikiPageServiceOverride/src/main/java/com/liferay/mentions/internal/service/MentionsWikiPageServiceWrapper.java
+++ modules/WikiPageServiceOverride/src/main/java/com/liferay/mentions/internal/service/MentionsWikiPageServiceWrapper.java
@@ -17,26 +17,50 @@ package com.liferay.mentions.internal.service;
 import com.liferay.mentions.configuration.MentionsGroupServiceConfiguration;
 import com.liferay.mentions.util.MentionsNotifier;
 import com.liferay.mentions.util.MentionsUtil;
+import com.liferay.portal.kernel.diff.DiffHtmlUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
+import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.settings.GroupServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.LocalizedValuesMap;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.NotificationThreadLocal;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.SubscriptionSender;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.wiki.configuration.WikiGroupServiceOverriddenConfiguration;
+import com.liferay.wiki.constants.WikiConstants;
+import com.liferay.wiki.constants.WikiPortletKeys;
+import com.liferay.wiki.engine.impl.WikiEngineRenderer;
+import com.liferay.wiki.model.WikiNode;
 import com.liferay.wiki.model.WikiPage;
+import com.liferay.wiki.model.WikiPageConstants;
 import com.liferay.wiki.service.WikiPageLocalService;
 import com.liferay.wiki.service.WikiPageLocalServiceWrapper;
+import com.liferay.wiki.util.WikiUtil;
 
 import java.io.Serializable;
 
 import java.util.Map;
+import java.util.Objects;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Norbert Kocsis
+ * @author Tamas Molnar
  */
 @Component(immediate = true, service = ServiceWrapper.class)
 public class MentionsWikiPageServiceWrapper
@@ -63,6 +87,28 @@ public class MentionsWikiPageServiceWrapper
 
 		page = super.updateStatus(
 			userId, page, status, serviceContext, workflowContext);
+
+		WikiGroupServiceOverriddenConfiguration
+			wikiGroupServiceOverriddenConfiguration =
+				_configurationProvider.getConfiguration(
+					WikiGroupServiceOverriddenConfiguration.class,
+					new GroupServiceSettingsLocator(
+						page.getGroupId(), WikiConstants.SERVICE_NAME));
+
+		// Subscriptions
+
+		if (status == WorkflowConstants.STATUS_APPROVED) {
+			if (NotificationThreadLocal.isEnabled() &&
+				(!page.isMinorEdit() ||
+				 wikiGroupServiceOverriddenConfiguration.
+					 pageMinorEditSendEmail())) {
+
+				notifySubscribers(
+					userId, page,
+					(String)workflowContext.get(WorkflowConstants.CONTEXT_URL),
+					serviceContext);
+			}
+		}
 
 		if ((status != WorkflowConstants.STATUS_APPROVED) ||
 			(oldStatus == WorkflowConstants.STATUS_IN_TRASH)) {
@@ -97,6 +143,201 @@ public class MentionsWikiPageServiceWrapper
 		return page;
 	}
 
+	protected String getDiffsURL(
+			WikiPage page, WikiPage previousVersionPage,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		if (previousVersionPage == null) {
+			return StringPool.BLANK;
+		}
+
+		HttpServletRequest request = serviceContext.getRequest();
+
+		if (request == null) {
+			return StringPool.BLANK;
+		}
+
+		PortletURL portletURL = null;
+
+		long plid = serviceContext.getPlid();
+
+		if (plid == LayoutConstants.DEFAULT_PLID) {
+			portletURL = PortalUtil.getControlPanelPortletURL(
+				request, WikiPortletKeys.WIKI_ADMIN,
+				PortletRequest.RENDER_PHASE);
+		}
+		else {
+			portletURL = PortletURLFactoryUtil.create(
+				request, WikiConstants.SERVICE_NAME, plid,
+				PortletRequest.RENDER_PHASE);
+		}
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "/wiki/compare_versions");
+		portletURL.setParameter("nodeId", String.valueOf(page.getNodeId()));
+		portletURL.setParameter("title", page.getTitle());
+		portletURL.setParameter(
+			"sourceVersion", String.valueOf(previousVersionPage.getVersion()));
+		portletURL.setParameter(
+			"targetVersion", String.valueOf(page.getVersion()));
+		portletURL.setParameter("type", "html");
+
+		return portletURL.toString();
+	}
+
+	protected void notifySubscribers(
+			long userId, WikiPage page, String pageURL,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		if (!page.isApproved() || Validator.isNull(pageURL)) {
+			return;
+		}
+
+		WikiGroupServiceOverriddenConfiguration
+			wikiGroupServiceOverriddenConfiguration =
+				_configurationProvider.getConfiguration(
+					WikiGroupServiceOverriddenConfiguration.class,
+					new GroupServiceSettingsLocator(
+						page.getGroupId(), WikiConstants.SERVICE_NAME));
+
+		boolean update = false;
+
+		if (page.getVersion() > WikiPageConstants.VERSION_DEFAULT) {
+			update = true;
+		}
+
+		if (!update &&
+			wikiGroupServiceOverriddenConfiguration.emailPageAddedEnabled()) {
+		}
+		else if (update &&
+				 wikiGroupServiceOverriddenConfiguration.
+					 emailPageUpdatedEnabled()) {
+		}
+		else {
+			return;
+		}
+
+		String portalURL = serviceContext.getPortalURL();
+
+		WikiPage previousVersionPage = getPreviousVersionPage(page);
+
+		String attachmentURLPrefix = WikiUtil.getAttachmentURLPrefix(
+			serviceContext.getPathMain(), serviceContext.getPlid(),
+			page.getNodeId(), page.getTitle());
+
+		attachmentURLPrefix = portalURL + attachmentURLPrefix;
+
+		String pageDiffs = StringPool.BLANK;
+
+		try {
+			pageDiffs = _wikiEngineRenderer.diffHtml(
+				previousVersionPage, page, null, null, attachmentURLPrefix);
+		}
+		catch (Exception e) {
+		}
+
+		String pageContent = null;
+
+		if (Objects.equals(page.getFormat(), "creole")) {
+			pageContent = _wikiEngineRenderer.convert(
+				page, null, null, attachmentURLPrefix);
+		}
+		else {
+			pageContent = page.getContent();
+			pageContent = WikiUtil.processContent(pageContent);
+		}
+
+		String pageTitle = page.getTitle();
+
+		String fromName =
+			wikiGroupServiceOverriddenConfiguration.emailFromName();
+		String fromAddress =
+			wikiGroupServiceOverriddenConfiguration.emailFromAddress();
+
+		LocalizedValuesMap subjectLocalizedValuesMap = null;
+		LocalizedValuesMap bodyLocalizedValuesMap = null;
+
+		if (update) {
+			subjectLocalizedValuesMap =
+				wikiGroupServiceOverriddenConfiguration.
+					emailPageUpdatedSubject();
+			bodyLocalizedValuesMap =
+				wikiGroupServiceOverriddenConfiguration.emailPageUpdatedBody();
+		}
+		else {
+			subjectLocalizedValuesMap =
+				wikiGroupServiceOverriddenConfiguration.emailPageAddedSubject();
+			bodyLocalizedValuesMap =
+				wikiGroupServiceOverriddenConfiguration.emailPageAddedBody();
+		}
+
+		SubscriptionSender subscriptionSender = new SubscriptionSender();
+
+		subscriptionSender.setClassName(page.getModelClassName());
+		subscriptionSender.setClassPK(page.getPageId());
+		subscriptionSender.setCompanyId(page.getCompanyId());
+		subscriptionSender.setContextAttribute(
+			"[$PAGE_CONTENT$]", pageContent, false);
+		subscriptionSender.setContextAttribute(
+			"[$PAGE_DIFFS$]", DiffHtmlUtil.replaceStyles(pageDiffs), false);
+
+		WikiNode node = page.getNode();
+
+		subscriptionSender.setContextAttributes(
+			"[$DIFFS_URL$]",
+			getDiffsURL(page, previousVersionPage, serviceContext),
+			"[$NODE_NAME$]", node.getName(), "[$PAGE_DATE_UPDATE$]",
+			page.getModifiedDate(), "[$PAGE_ID$]", page.getPageId(),
+			"[$PAGE_SUMMARY$]", page.getSummary(), "[$PAGE_TITLE$]", pageTitle,
+			"[$PAGE_URL$]", pageURL);
+
+		subscriptionSender.setContextCreatorUserPrefix("PAGE");
+		subscriptionSender.setCreatorUserId(page.getUserId());
+		subscriptionSender.setCurrentUserId(userId);
+		subscriptionSender.setEntryTitle(pageTitle);
+		subscriptionSender.setEntryURL(pageURL);
+		subscriptionSender.setFrom(fromAddress, fromName);
+		subscriptionSender.setHtmlFormat(true);
+
+		if (bodyLocalizedValuesMap != null) {
+			subscriptionSender.setLocalizedBodyMap(
+				LocalizationUtil.getMap(bodyLocalizedValuesMap));
+		}
+
+		if (subjectLocalizedValuesMap != null) {
+			subscriptionSender.setLocalizedSubjectMap(
+				LocalizationUtil.getMap(subjectLocalizedValuesMap));
+		}
+
+		subscriptionSender.setMailId(
+			"wiki_page", page.getNodeId(), page.getPageId());
+
+		int notificationType =
+			UserNotificationDefinition.NOTIFICATION_TYPE_ADD_ENTRY;
+
+		if (update) {
+			notificationType =
+				UserNotificationDefinition.NOTIFICATION_TYPE_UPDATE_ENTRY;
+		}
+
+		subscriptionSender.setNotificationType(notificationType);
+
+		subscriptionSender.setPortletId(WikiPortletKeys.WIKI);
+		subscriptionSender.setReplyToAddress(fromAddress);
+		subscriptionSender.setScopeGroupId(page.getGroupId());
+		subscriptionSender.setServiceContext(serviceContext);
+
+		subscriptionSender.addPersistedSubscribers(
+			WikiNode.class.getName(), page.getNodeId());
+
+		subscriptionSender.addPersistedSubscribers(
+			WikiPage.class.getName(), page.getResourcePrimKey());
+
+		subscriptionSender.flushNotificationsAsync();
+	}
+
 	@Reference(unbind = "-")
 	protected void setConfigurationProvider(
 		ConfigurationProvider configurationProvider) {
@@ -110,6 +351,13 @@ public class MentionsWikiPageServiceWrapper
 	}
 
 	@Reference(unbind = "-")
+	protected void setWikiEngineRenderer(
+		WikiEngineRenderer wikiEngineRenderer) {
+
+		_wikiEngineRenderer = wikiEngineRenderer;
+	}
+
+	@Reference(unbind = "-")
 	protected void setWikiPageLocalService(
 		WikiPageLocalService wikiPageLocalService) {
 
@@ -118,6 +366,7 @@ public class MentionsWikiPageServiceWrapper
 
 	private ConfigurationProvider _configurationProvider;
 	private MentionsNotifier _mentionsNotifier;
+	private WikiEngineRenderer _wikiEngineRenderer;
 	private WikiPageLocalService _wikiPageLocalService;
 
 }
