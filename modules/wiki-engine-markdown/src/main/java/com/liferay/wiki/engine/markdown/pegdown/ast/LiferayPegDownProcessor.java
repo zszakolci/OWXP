1c25c0504f9119e1570d8fd986aabe3cd36e2c9d
NorbertKocsis <norbert.kocsis@liferay.com>
2016-11-08 16:35:13 +0100

    OWXP-135 Strip anchor id tags from final HTML

--- modules/wiki-engine-markdown/src/main/java/com/liferay/wiki/engine/markdown/pegdown/ast/LiferayPegDownProcessor.java
+++ modules/wiki-engine-markdown/src/main/java/com/liferay/wiki/engine/markdown/pegdown/ast/LiferayPegDownProcessor.java
@@ -7,6 +7,12 @@ import org.pegdown.Parser;
 import org.pegdown.PegDownProcessor;
 import org.pegdown.ast.RootNode;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
 public class LiferayPegDownProcessor extends PegDownProcessor {
 
 	public LiferayPegDownProcessor(Parser parser) {
@@ -22,6 +28,69 @@ public class LiferayPegDownProcessor extends PegDownProcessor {
 		LiferayToHtmlSerializer liferayToHtmlSerializer =
 			new LiferayToHtmlSerializer(linkRenderer);
 
-		return liferayToHtmlSerializer.toHtml(rootNode);
+		String html = liferayToHtmlSerializer.toHtml(rootNode);
+
+		return stripIds(html); 
 	}
+
+	protected String stripIds(String content) {
+		int index = content.indexOf("[](id=");
+
+		if (index == -1) {
+			return content;
+		}
+
+		StringBundler sb = new StringBundler();
+
+		do {
+			int x = content.indexOf(StringPool.EQUAL, index);
+
+			int y = content.indexOf(StringPool.CLOSE_PARENTHESIS, x);
+
+			if (y != -1) {
+				int z = content.indexOf("</h", y);
+
+				if (z != (y + 1)) {
+					sb.append(content.substring(0, y + 1));
+				}
+				else {
+					sb.append(
+						StringUtil.trimTrailing(content.substring(0, index)));
+				}
+
+				content = content.substring(y + 1);
+			}
+			else {
+				if (_log.isWarnEnabled()) {
+					String msg = content.substring(index);
+
+					// Get the invalid id text from the content
+
+					int spaceIndex = content.indexOf(StringPool.SPACE);
+
+					if (spaceIndex != -1) {
+						msg = content.substring(index, spaceIndex);
+					}
+
+					_log.warn(
+						"Missing ')' for web content containing header id " +
+							msg);
+				}
+
+				// Since no close parenthesis remains in the content, stop
+				// stripping out IDs and simply include all of the remaining
+				// content
+
+				break;
+			}
+		}
+		while ((index = content.indexOf("[](id=")) != -1);
+
+		sb.append(content);
+
+		return sb.toString();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+			LiferayPegDownProcessor.class);
 }
