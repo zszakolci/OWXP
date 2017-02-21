2f3b28fc9076679babdda76e5622438e3b232e27
hudakl <laszlo.hudak@liferay.com>
2017-02-21 14:36:24 +0100

    OWXP-213 Genreate decision services

---
+++ modules/micro-maintainance-decisison/micro-maintainance-decisison-api/src/main/java/com/liferay/micro/maintainance/decision/model/DecisionEntryWrapper.java
@@ -0,0 +1,536 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.micro.maintainance.decision.model;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.expando.kernel.model.ExpandoBridge;
+
+import com.liferay.exportimport.kernel.lar.StagedModelType;
+
+import com.liferay.portal.kernel.model.ModelWrapper;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+import java.io.Serializable;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * <p>
+ * This class is a wrapper for {@link DecisionEntry}.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see DecisionEntry
+ * @generated
+ */
+@ProviderType
+public class DecisionEntryWrapper implements DecisionEntry,
+	ModelWrapper<DecisionEntry> {
+	public DecisionEntryWrapper(DecisionEntry decisionEntry) {
+		_decisionEntry = decisionEntry;
+	}
+
+	@Override
+	public Class<?> getModelClass() {
+		return DecisionEntry.class;
+	}
+
+	@Override
+	public String getModelClassName() {
+		return DecisionEntry.class.getName();
+	}
+
+	@Override
+	public Map<String, Object> getModelAttributes() {
+		Map<String, Object> attributes = new HashMap<String, Object>();
+
+		attributes.put("uuid", getUuid());
+		attributes.put("decisionId", getDecisionId());
+		attributes.put("companyId", getCompanyId());
+		attributes.put("userId", getUserId());
+		attributes.put("userName", getUserName());
+		attributes.put("createDate", getCreateDate());
+		attributes.put("modifiedDate", getModifiedDate());
+		attributes.put("analysisId", getAnalysisId());
+		attributes.put("outcome", getOutcome());
+		attributes.put("handled", getHandled());
+
+		return attributes;
+	}
+
+	@Override
+	public void setModelAttributes(Map<String, Object> attributes) {
+		String uuid = (String)attributes.get("uuid");
+
+		if (uuid != null) {
+			setUuid(uuid);
+		}
+
+		Long decisionId = (Long)attributes.get("decisionId");
+
+		if (decisionId != null) {
+			setDecisionId(decisionId);
+		}
+
+		Long companyId = (Long)attributes.get("companyId");
+
+		if (companyId != null) {
+			setCompanyId(companyId);
+		}
+
+		Long userId = (Long)attributes.get("userId");
+
+		if (userId != null) {
+			setUserId(userId);
+		}
+
+		String userName = (String)attributes.get("userName");
+
+		if (userName != null) {
+			setUserName(userName);
+		}
+
+		Date createDate = (Date)attributes.get("createDate");
+
+		if (createDate != null) {
+			setCreateDate(createDate);
+		}
+
+		Date modifiedDate = (Date)attributes.get("modifiedDate");
+
+		if (modifiedDate != null) {
+			setModifiedDate(modifiedDate);
+		}
+
+		Long analysisId = (Long)attributes.get("analysisId");
+
+		if (analysisId != null) {
+			setAnalysisId(analysisId);
+		}
+
+		String outcome = (String)attributes.get("outcome");
+
+		if (outcome != null) {
+			setOutcome(outcome);
+		}
+
+		Boolean handled = (Boolean)attributes.get("handled");
+
+		if (handled != null) {
+			setHandled(handled);
+		}
+	}
+
+	@Override
+	public DecisionEntry toEscapedModel() {
+		return new DecisionEntryWrapper(_decisionEntry.toEscapedModel());
+	}
+
+	@Override
+	public DecisionEntry toUnescapedModel() {
+		return new DecisionEntryWrapper(_decisionEntry.toUnescapedModel());
+	}
+
+	/**
+	* Returns the handled of this decision entry.
+	*
+	* @return the handled of this decision entry
+	*/
+	@Override
+	public boolean getHandled() {
+		return _decisionEntry.getHandled();
+	}
+
+	@Override
+	public boolean isCachedModel() {
+		return _decisionEntry.isCachedModel();
+	}
+
+	@Override
+	public boolean isEscapedModel() {
+		return _decisionEntry.isEscapedModel();
+	}
+
+	/**
+	* Returns <code>true</code> if this decision entry is handled.
+	*
+	* @return <code>true</code> if this decision entry is handled; <code>false</code> otherwise
+	*/
+	@Override
+	public boolean isHandled() {
+		return _decisionEntry.isHandled();
+	}
+
+	@Override
+	public boolean isNew() {
+		return _decisionEntry.isNew();
+	}
+
+	@Override
+	public ExpandoBridge getExpandoBridge() {
+		return _decisionEntry.getExpandoBridge();
+	}
+
+	@Override
+	public com.liferay.portal.kernel.model.CacheModel<DecisionEntry> toCacheModel() {
+		return _decisionEntry.toCacheModel();
+	}
+
+	@Override
+	public int compareTo(DecisionEntry decisionEntry) {
+		return _decisionEntry.compareTo(decisionEntry);
+	}
+
+	@Override
+	public int hashCode() {
+		return _decisionEntry.hashCode();
+	}
+
+	@Override
+	public Serializable getPrimaryKeyObj() {
+		return _decisionEntry.getPrimaryKeyObj();
+	}
+
+	@Override
+	public java.lang.Object clone() {
+		return new DecisionEntryWrapper((DecisionEntry)_decisionEntry.clone());
+	}
+
+	/**
+	* Returns the outcome of this decision entry.
+	*
+	* @return the outcome of this decision entry
+	*/
+	@Override
+	public java.lang.String getOutcome() {
+		return _decisionEntry.getOutcome();
+	}
+
+	/**
+	* Returns the user name of this decision entry.
+	*
+	* @return the user name of this decision entry
+	*/
+	@Override
+	public java.lang.String getUserName() {
+		return _decisionEntry.getUserName();
+	}
+
+	/**
+	* Returns the user uuid of this decision entry.
+	*
+	* @return the user uuid of this decision entry
+	*/
+	@Override
+	public java.lang.String getUserUuid() {
+		return _decisionEntry.getUserUuid();
+	}
+
+	/**
+	* Returns the uuid of this decision entry.
+	*
+	* @return the uuid of this decision entry
+	*/
+	@Override
+	public java.lang.String getUuid() {
+		return _decisionEntry.getUuid();
+	}
+
+	@Override
+	public java.lang.String toString() {
+		return _decisionEntry.toString();
+	}
+
+	@Override
+	public java.lang.String toXmlString() {
+		return _decisionEntry.toXmlString();
+	}
+
+	/**
+	* Returns the create date of this decision entry.
+	*
+	* @return the create date of this decision entry
+	*/
+	@Override
+	public Date getCreateDate() {
+		return _decisionEntry.getCreateDate();
+	}
+
+	/**
+	* Returns the modified date of this decision entry.
+	*
+	* @return the modified date of this decision entry
+	*/
+	@Override
+	public Date getModifiedDate() {
+		return _decisionEntry.getModifiedDate();
+	}
+
+	/**
+	* Returns the analysis ID of this decision entry.
+	*
+	* @return the analysis ID of this decision entry
+	*/
+	@Override
+	public long getAnalysisId() {
+		return _decisionEntry.getAnalysisId();
+	}
+
+	/**
+	* Returns the company ID of this decision entry.
+	*
+	* @return the company ID of this decision entry
+	*/
+	@Override
+	public long getCompanyId() {
+		return _decisionEntry.getCompanyId();
+	}
+
+	/**
+	* Returns the decision ID of this decision entry.
+	*
+	* @return the decision ID of this decision entry
+	*/
+	@Override
+	public long getDecisionId() {
+		return _decisionEntry.getDecisionId();
+	}
+
+	/**
+	* Returns the primary key of this decision entry.
+	*
+	* @return the primary key of this decision entry
+	*/
+	@Override
+	public long getPrimaryKey() {
+		return _decisionEntry.getPrimaryKey();
+	}
+
+	/**
+	* Returns the user ID of this decision entry.
+	*
+	* @return the user ID of this decision entry
+	*/
+	@Override
+	public long getUserId() {
+		return _decisionEntry.getUserId();
+	}
+
+	@Override
+	public void persist() {
+		_decisionEntry.persist();
+	}
+
+	/**
+	* Sets the analysis ID of this decision entry.
+	*
+	* @param analysisId the analysis ID of this decision entry
+	*/
+	@Override
+	public void setAnalysisId(long analysisId) {
+		_decisionEntry.setAnalysisId(analysisId);
+	}
+
+	@Override
+	public void setCachedModel(boolean cachedModel) {
+		_decisionEntry.setCachedModel(cachedModel);
+	}
+
+	/**
+	* Sets the company ID of this decision entry.
+	*
+	* @param companyId the company ID of this decision entry
+	*/
+	@Override
+	public void setCompanyId(long companyId) {
+		_decisionEntry.setCompanyId(companyId);
+	}
+
+	/**
+	* Sets the create date of this decision entry.
+	*
+	* @param createDate the create date of this decision entry
+	*/
+	@Override
+	public void setCreateDate(Date createDate) {
+		_decisionEntry.setCreateDate(createDate);
+	}
+
+	/**
+	* Sets the decision ID of this decision entry.
+	*
+	* @param decisionId the decision ID of this decision entry
+	*/
+	@Override
+	public void setDecisionId(long decisionId) {
+		_decisionEntry.setDecisionId(decisionId);
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(ExpandoBridge expandoBridge) {
+		_decisionEntry.setExpandoBridgeAttributes(expandoBridge);
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(
+		com.liferay.portal.kernel.model.BaseModel<?> baseModel) {
+		_decisionEntry.setExpandoBridgeAttributes(baseModel);
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(ServiceContext serviceContext) {
+		_decisionEntry.setExpandoBridgeAttributes(serviceContext);
+	}
+
+	/**
+	* Sets whether this decision entry is handled.
+	*
+	* @param handled the handled of this decision entry
+	*/
+	@Override
+	public void setHandled(boolean handled) {
+		_decisionEntry.setHandled(handled);
+	}
+
+	/**
+	* Sets the modified date of this decision entry.
+	*
+	* @param modifiedDate the modified date of this decision entry
+	*/
+	@Override
+	public void setModifiedDate(Date modifiedDate) {
+		_decisionEntry.setModifiedDate(modifiedDate);
+	}
+
+	@Override
+	public void setNew(boolean n) {
+		_decisionEntry.setNew(n);
+	}
+
+	/**
+	* Sets the outcome of this decision entry.
+	*
+	* @param outcome the outcome of this decision entry
+	*/
+	@Override
+	public void setOutcome(java.lang.String outcome) {
+		_decisionEntry.setOutcome(outcome);
+	}
+
+	/**
+	* Sets the primary key of this decision entry.
+	*
+	* @param primaryKey the primary key of this decision entry
+	*/
+	@Override
+	public void setPrimaryKey(long primaryKey) {
+		_decisionEntry.setPrimaryKey(primaryKey);
+	}
+
+	@Override
+	public void setPrimaryKeyObj(Serializable primaryKeyObj) {
+		_decisionEntry.setPrimaryKeyObj(primaryKeyObj);
+	}
+
+	/**
+	* Sets the user ID of this decision entry.
+	*
+	* @param userId the user ID of this decision entry
+	*/
+	@Override
+	public void setUserId(long userId) {
+		_decisionEntry.setUserId(userId);
+	}
+
+	/**
+	* Sets the user name of this decision entry.
+	*
+	* @param userName the user name of this decision entry
+	*/
+	@Override
+	public void setUserName(java.lang.String userName) {
+		_decisionEntry.setUserName(userName);
+	}
+
+	/**
+	* Sets the user uuid of this decision entry.
+	*
+	* @param userUuid the user uuid of this decision entry
+	*/
+	@Override
+	public void setUserUuid(java.lang.String userUuid) {
+		_decisionEntry.setUserUuid(userUuid);
+	}
+
+	/**
+	* Sets the uuid of this decision entry.
+	*
+	* @param uuid the uuid of this decision entry
+	*/
+	@Override
+	public void setUuid(java.lang.String uuid) {
+		_decisionEntry.setUuid(uuid);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof DecisionEntryWrapper)) {
+			return false;
+		}
+
+		DecisionEntryWrapper decisionEntryWrapper = (DecisionEntryWrapper)obj;
+
+		if (Objects.equals(_decisionEntry, decisionEntryWrapper._decisionEntry)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public StagedModelType getStagedModelType() {
+		return _decisionEntry.getStagedModelType();
+	}
+
+	@Override
+	public DecisionEntry getWrappedModel() {
+		return _decisionEntry;
+	}
+
+	@Override
+	public boolean isEntityCacheEnabled() {
+		return _decisionEntry.isEntityCacheEnabled();
+	}
+
+	@Override
+	public boolean isFinderCacheEnabled() {
+		return _decisionEntry.isFinderCacheEnabled();
+	}
+
+	@Override
+	public void resetOriginalValues() {
+		_decisionEntry.resetOriginalValues();
+	}
+
+	private final DecisionEntry _decisionEntry;
+}
---
+++ modules/micro-maintainance-decisison/micro-maintainance-decisison-api/src/main/java/com/liferay/micro/maintainance/decision/service/DecisionEntryLocalServiceWrapper.java
@@ -0,0 +1,316 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.micro.maintainance.decision.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.service.ServiceWrapper;
+
+/**
+ * Provides a wrapper for {@link DecisionEntryLocalService}.
+ *
+ * @author Brian Wing Shun Chan
+ * @see DecisionEntryLocalService
+ * @generated
+ */
+@ProviderType
+public class DecisionEntryLocalServiceWrapper
+	implements DecisionEntryLocalService,
+		ServiceWrapper<DecisionEntryLocalService> {
+	public DecisionEntryLocalServiceWrapper(
+		DecisionEntryLocalService decisionEntryLocalService) {
+		_decisionEntryLocalService = decisionEntryLocalService;
+	}
+
+	/**
+	* Adds the decision entry to the database. Also notifies the appropriate model listeners.
+	*
+	* @param decisionEntry the decision entry
+	* @return the decision entry that was added
+	*/
+	@Override
+	public com.liferay.micro.maintainance.decision.model.DecisionEntry addDecisionEntry(
+		com.liferay.micro.maintainance.decision.model.DecisionEntry decisionEntry) {
+		return _decisionEntryLocalService.addDecisionEntry(decisionEntry);
+	}
+
+	/**
+	* Creates a new decision entry with the primary key. Does not add the decision entry to the database.
+	*
+	* @param decisionId the primary key for the new decision entry
+	* @return the new decision entry
+	*/
+	@Override
+	public com.liferay.micro.maintainance.decision.model.DecisionEntry createDecisionEntry(
+		long decisionId) {
+		return _decisionEntryLocalService.createDecisionEntry(decisionId);
+	}
+
+	/**
+	* Deletes the decision entry from the database. Also notifies the appropriate model listeners.
+	*
+	* @param decisionEntry the decision entry
+	* @return the decision entry that was removed
+	*/
+	@Override
+	public com.liferay.micro.maintainance.decision.model.DecisionEntry deleteDecisionEntry(
+		com.liferay.micro.maintainance.decision.model.DecisionEntry decisionEntry) {
+		return _decisionEntryLocalService.deleteDecisionEntry(decisionEntry);
+	}
+
+	/**
+	* Deletes the decision entry with the primary key from the database. Also notifies the appropriate model listeners.
+	*
+	* @param decisionId the primary key of the decision entry
+	* @return the decision entry that was removed
+	* @throws PortalException if a decision entry with the primary key could not be found
+	*/
+	@Override
+	public com.liferay.micro.maintainance.decision.model.DecisionEntry deleteDecisionEntry(
+		long decisionId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _decisionEntryLocalService.deleteDecisionEntry(decisionId);
+	}
+
+	@Override
+	public com.liferay.micro.maintainance.decision.model.DecisionEntry fetchDecisionEntry(
+		long decisionId) {
+		return _decisionEntryLocalService.fetchDecisionEntry(decisionId);
+	}
+
+	/**
+	* Returns the decision entry with the matching UUID and company.
+	*
+	* @param uuid the decision entry's UUID
+	* @param companyId the primary key of the company
+	* @return the matching decision entry, or <code>null</code> if a matching decision entry could not be found
+	*/
+	@Override
+	public com.liferay.micro.maintainance.decision.model.DecisionEntry fetchDecisionEntryByUuidAndCompanyId(
+		java.lang.String uuid, long companyId) {
+		return _decisionEntryLocalService.fetchDecisionEntryByUuidAndCompanyId(uuid,
+			companyId);
+	}
+
+	/**
+	* Returns the decision entry with the primary key.
+	*
+	* @param decisionId the primary key of the decision entry
+	* @return the decision entry
+	* @throws PortalException if a decision entry with the primary key could not be found
+	*/
+	@Override
+	public com.liferay.micro.maintainance.decision.model.DecisionEntry getDecisionEntry(
+		long decisionId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _decisionEntryLocalService.getDecisionEntry(decisionId);
+	}
+
+	/**
+	* Returns the decision entry with the matching UUID and company.
+	*
+	* @param uuid the decision entry's UUID
+	* @param companyId the primary key of the company
+	* @return the matching decision entry
+	* @throws PortalException if a matching decision entry could not be found
+	*/
+	@Override
+	public com.liferay.micro.maintainance.decision.model.DecisionEntry getDecisionEntryByUuidAndCompanyId(
+		java.lang.String uuid, long companyId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _decisionEntryLocalService.getDecisionEntryByUuidAndCompanyId(uuid,
+			companyId);
+	}
+
+	/**
+	* Updates the decision entry in the database or adds it if it does not yet exist. Also notifies the appropriate model listeners.
+	*
+	* @param decisionEntry the decision entry
+	* @return the decision entry that was updated
+	*/
+	@Override
+	public com.liferay.micro.maintainance.decision.model.DecisionEntry updateDecisionEntry(
+		com.liferay.micro.maintainance.decision.model.DecisionEntry decisionEntry) {
+		return _decisionEntryLocalService.updateDecisionEntry(decisionEntry);
+	}
+
+	@Override
+	public com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery getActionableDynamicQuery() {
+		return _decisionEntryLocalService.getActionableDynamicQuery();
+	}
+
+	@Override
+	public com.liferay.portal.kernel.dao.orm.DynamicQuery dynamicQuery() {
+		return _decisionEntryLocalService.dynamicQuery();
+	}
+
+	@Override
+	public com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery getExportActionableDynamicQuery(
+		com.liferay.exportimport.kernel.lar.PortletDataContext portletDataContext) {
+		return _decisionEntryLocalService.getExportActionableDynamicQuery(portletDataContext);
+	}
+
+	@Override
+	public com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery getIndexableActionableDynamicQuery() {
+		return _decisionEntryLocalService.getIndexableActionableDynamicQuery();
+	}
+
+	/**
+	* @throws PortalException
+	*/
+	@Override
+	public com.liferay.portal.kernel.model.PersistedModel deletePersistedModel(
+		com.liferay.portal.kernel.model.PersistedModel persistedModel)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _decisionEntryLocalService.deletePersistedModel(persistedModel);
+	}
+
+	@Override
+	public com.liferay.portal.kernel.model.PersistedModel getPersistedModel(
+		java.io.Serializable primaryKeyObj)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _decisionEntryLocalService.getPersistedModel(primaryKeyObj);
+	}
+
+	/**
+	* Returns the number of decision entries.
+	*
+	* @return the number of decision entries
+	*/
+	@Override
+	public int getDecisionEntriesCount() {
+		return _decisionEntryLocalService.getDecisionEntriesCount();
+	}
+
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	@Override
+	public java.lang.String getOSGiServiceIdentifier() {
+		return _decisionEntryLocalService.getOSGiServiceIdentifier();
+	}
+
+	/**
+	* Performs a dynamic query on the database and returns the matching rows.
+	*
+	* @param dynamicQuery the dynamic query
+	* @return the matching rows
+	*/
+	@Override
+	public <T> java.util.List<T> dynamicQuery(
+		com.liferay.portal.kernel.dao.orm.DynamicQuery dynamicQuery) {
+		return _decisionEntryLocalService.dynamicQuery(dynamicQuery);
+	}
+
+	/**
+	* Performs a dynamic query on the database and returns a range of the matching rows.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link com.liferay.micro.maintainance.decision.model.impl.DecisionEntryModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param dynamicQuery the dynamic query
+	* @param start the lower bound of the range of model instances
+	* @param end the upper bound of the range of model instances (not inclusive)
+	* @return the range of matching rows
+	*/
+	@Override
+	public <T> java.util.List<T> dynamicQuery(
+		com.liferay.portal.kernel.dao.orm.DynamicQuery dynamicQuery, int start,
+		int end) {
+		return _decisionEntryLocalService.dynamicQuery(dynamicQuery, start, end);
+	}
+
+	/**
+	* Performs a dynamic query on the database and returns an ordered range of the matching rows.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link com.liferay.micro.maintainance.decision.model.impl.DecisionEntryModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param dynamicQuery the dynamic query
+	* @param start the lower bound of the range of model instances
+	* @param end the upper bound of the range of model instances (not inclusive)
+	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	* @return the ordered range of matching rows
+	*/
+	@Override
+	public <T> java.util.List<T> dynamicQuery(
+		com.liferay.portal.kernel.dao.orm.DynamicQuery dynamicQuery, int start,
+		int end,
+		com.liferay.portal.kernel.util.OrderByComparator<T> orderByComparator) {
+		return _decisionEntryLocalService.dynamicQuery(dynamicQuery, start,
+			end, orderByComparator);
+	}
+
+	/**
+	* Returns a range of all the decision entries.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link com.liferay.micro.maintainance.decision.model.impl.DecisionEntryModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param start the lower bound of the range of decision entries
+	* @param end the upper bound of the range of decision entries (not inclusive)
+	* @return the range of decision entries
+	*/
+	@Override
+	public java.util.List<com.liferay.micro.maintainance.decision.model.DecisionEntry> getDecisionEntries(
+		int start, int end) {
+		return _decisionEntryLocalService.getDecisionEntries(start, end);
+	}
+
+	/**
+	* Returns the number of rows matching the dynamic query.
+	*
+	* @param dynamicQuery the dynamic query
+	* @return the number of rows matching the dynamic query
+	*/
+	@Override
+	public long dynamicQueryCount(
+		com.liferay.portal.kernel.dao.orm.DynamicQuery dynamicQuery) {
+		return _decisionEntryLocalService.dynamicQueryCount(dynamicQuery);
+	}
+
+	/**
+	* Returns the number of rows matching the dynamic query.
+	*
+	* @param dynamicQuery the dynamic query
+	* @param projection the projection to apply to the query
+	* @return the number of rows matching the dynamic query
+	*/
+	@Override
+	public long dynamicQueryCount(
+		com.liferay.portal.kernel.dao.orm.DynamicQuery dynamicQuery,
+		com.liferay.portal.kernel.dao.orm.Projection projection) {
+		return _decisionEntryLocalService.dynamicQueryCount(dynamicQuery,
+			projection);
+	}
+
+	@Override
+	public DecisionEntryLocalService getWrappedService() {
+		return _decisionEntryLocalService;
+	}
+
+	@Override
+	public void setWrappedService(
+		DecisionEntryLocalService decisionEntryLocalService) {
+		_decisionEntryLocalService = decisionEntryLocalService;
+	}
+
+	private DecisionEntryLocalService _decisionEntryLocalService;
+}
