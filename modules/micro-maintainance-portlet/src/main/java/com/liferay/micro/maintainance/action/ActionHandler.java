6dee8abb639be348d4dc9fc115e69fd0628a0e5a
hudakl <laszlo.hudak@liferay.com>
2017-03-06 13:58:57 +0100

    OWXP-213 Implementing Action handler

--- modules/micro-maintainance-portlet/src/main/java/com/liferay/micro/maintainance/action/ActionHandler.java
+++ modules/micro-maintainance-portlet/src/main/java/com/liferay/micro/maintainance/action/ActionHandler.java
@@ -1,5 +1,18 @@
 package com.liferay.micro.maintainance.action;
 
+import java.util.List;
+
+import com.liferay.micro.maintainance.analysis.model.AnalysisEntry;
+import com.liferay.micro.maintainance.analysis.service.AnalysisEntryLocalServiceUtil;
+
 public class ActionHandler {
 
+	public static void performActions(List<Action> actions, long analysisId) {
+		AnalysisEntry analysisEntry = 
+			AnalysisEntryLocalServiceUtil.fetchAnalysisEntry(analysisId);
+
+		for (Action action: actions) {
+			action.performAction(analysisEntry);
+		}
+	}
 }
