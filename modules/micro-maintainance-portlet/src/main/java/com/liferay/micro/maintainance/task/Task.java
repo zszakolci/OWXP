b703d8f8a89dbfa47985faeead9e4dca6596fa23
hudakl <laszlo.hudak@liferay.com>
2017-03-07 15:35:56 +0100

    OWXP-213 Add analyse criteria to the tasks

--- modules/micro-maintainance-portlet/src/main/java/com/liferay/micro/maintainance/task/Task.java
+++ modules/micro-maintainance-portlet/src/main/java/com/liferay/micro/maintainance/task/Task.java
@@ -3,6 +3,7 @@ package com.liferay.micro.maintainance.task;
 import java.util.List;
 
 import com.liferay.micro.maintainance.action.Action;
+import com.liferay.micro.maintainance.task.model.CandidateMaintenance;
 
 public interface Task {
 	public List<Action> analyze(long analysisId);
@@ -12,4 +13,6 @@ public interface Task {
 	public String getTaskName();
 
 	public void setTaskId(long taskId);
+
+	public boolean isAnalyseReady(CandidateMaintenance canMain);
 }
