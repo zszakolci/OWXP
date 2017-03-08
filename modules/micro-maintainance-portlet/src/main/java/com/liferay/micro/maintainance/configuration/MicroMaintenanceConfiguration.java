7f47d4e220fb18910f2372edee6cbf7ea018d497
hudakl <laszlo.hudak@liferay.com>
2017-03-08 13:20:55 +0100

    OWXP-213 Make the cron job for checking the votes configurable

--- modules/micro-maintainance-portlet/src/main/java/com/liferay/micro/maintainance/configuration/MicroMaintenanceConfiguration.java
+++ modules/micro-maintainance-portlet/src/main/java/com/liferay/micro/maintainance/configuration/MicroMaintenanceConfiguration.java
@@ -6,20 +6,9 @@ import aQute.bnd.annotation.metatype.Meta;
 public interface MicroMaintenanceConfiguration {
 
 	@Meta.AD(
-		deflt = "0",
-		required = true
-	)
-	public int checkingPeriodDays();
-
-	@Meta.AD(
 		deflt = "1",
 		required = true
 	)
 	public int checkingPeriodHours();
 
-	@Meta.AD(
-		deflt = "0",
-		required = true
-	)
-	public int checkingPeriodMinutes();
 }
