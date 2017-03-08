29a5aaa5a6fcb093b6faaa92507ef4c013b91a6c
hudakl <laszlo.hudak@liferay.com>
2017-03-08 14:04:02 +0100

    OWXP-213 Methods for the UI

---
+++ modules/micro-maintainance-portlet/src/main/java/com/liferay/micro/maintainance/util/VotesJSONSerializer.java
@@ -0,0 +1,12 @@
+package com.liferay.micro.maintainance.util;
+
+public class VotesJSONSerializer {
+
+	public static String createVotes() {
+		return "";
+	}
+
+	public static String updateVotes(String analysisData, int vote) {
+		return "";
+	}
+}
