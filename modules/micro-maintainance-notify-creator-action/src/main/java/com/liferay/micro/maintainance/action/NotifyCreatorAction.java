a80d7b6fc8822b2f241b59ec9a1883cabafe9d9a
hudakl <laszlo.hudak@liferay.com>
2017-03-15 14:42:19 +0100

    OWXP-213 Creating NotifyUserAction and OutdatedTask

---
+++ modules/micro-maintainance-notify-creator-action/src/main/java/com/liferay/micro/maintainance/action/NotifyCreatorAction.java
@@ -0,0 +1,13 @@
+package com.liferay.micro.maintainance.action;
+
+import com.liferay.micro.maintainance.analysis.model.AnalysisEntry;
+
+public class NotifyCreatorAction implements Action {
+
+	@Override
+	public boolean performAction(AnalysisEntry analysisEntry) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
