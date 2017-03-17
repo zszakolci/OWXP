5a97bfaf745ff82374425b0fa2e5155215d6d176
rimi saadou <rimi.saadou@liferay.com>
2017-03-17 15:28:53 +0100

    OWXP-213 Added NotifyCreatorAction logic

--- modules/micro-maintainance-notify-creator-action/src/main/java/com/liferay/micro/maintainance/action/NotifyCreatorAction.java
+++ modules/micro-maintainance-notify-creator-action/src/main/java/com/liferay/micro/maintainance/action/NotifyCreatorAction.java
@@ -1,13 +1,70 @@
 package com.liferay.micro.maintainance.action;
 
+import javax.mail.internet.InternetAddress;
+
+import com.liferay.mail.kernel.model.MailMessage;
+import com.liferay.mail.kernel.service.MailServiceUtil;
 import com.liferay.micro.maintainance.analysis.model.AnalysisEntry;
+import com.liferay.micro.maintainance.util.VoteCalculations;
+import com.liferay.micro.maintainance.util.WikiUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.wiki.model.WikiPage;
 
 public class NotifyCreatorAction implements Action {
 
 	@Override
 	public boolean performAction(AnalysisEntry analysisEntry) {
-		// TODO Auto-generated method stub
+
+		try {
+			long companyId = CompanyThreadLocal.getCompanyId();
+
+			WikiPage wikiPage = WikiUtil.getWikiPageByAnalysis(
+				analysisEntry.getCanMainId());
+
+			User user = UserLocalServiceUtil.getUser(wikiPage.getUserId());
+
+			User sender = UserLocalServiceUtil.getUserByScreenName(
+				companyId, "glados");
+
+			if(Validator.isNull(sender)) {
+				sender = UserLocalServiceUtil.getDefaultUser(companyId);
+			}
+
+			String body = VoteCalculations.toReadableFormat(
+				analysisEntry.getAnalysisData());
+
+			_sendMail(
+				sender.getEmailAddress(), sender.getFullName(),
+				user.getEmailAddress(), SUBJECT, body);
+
+			return true;
+		} catch (Exception px) {
+		}
+
 		return false;
 	}
 
+	private void _sendMail(
+			String fromAddress, String fromName, String toAddres,
+			String subject, String body) 
+		throws Exception {
+
+		InternetAddress fromInternetAddress = new InternetAddress(
+			fromAddress, fromName);
+
+		MailMessage mailMessage = new MailMessage(
+			fromInternetAddress, subject, body, true);
+
+		InternetAddress toInternetAddress = new InternetAddress(
+			toAddres);
+
+		mailMessage.setTo(toInternetAddress);
+
+		MailServiceUtil.sendEmail(mailMessage);
+	}
+
+	private final static String SUBJECT = "Outdated page";
 }
