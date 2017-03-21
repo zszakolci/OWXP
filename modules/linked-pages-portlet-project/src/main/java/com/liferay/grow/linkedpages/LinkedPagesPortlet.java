b484d263ef940533f7b83ea82ac8ff7b9754d4de
Tamas Molnar <tamas.molnar@liferay.com>
2017-03-20 18:21:36 -0700

    OWXP-134 SF - Copyright + author

--- modules/linked-pages-portlet-project/src/main/java/com/liferay/grow/linkedpages/LinkedPagesPortlet.java
+++ modules/linked-pages-portlet-project/src/main/java/com/liferay/grow/linkedpages/LinkedPagesPortlet.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.grow.linkedpages;
 
 import com.liferay.grow.linkedpages.util.LinkedPagesPortletKeys;
@@ -6,6 +20,10 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import javax.portlet.Portlet;
 
 import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Laszlo Hudak
+ */
 @Component(
 	immediate = true,
 	property = {
