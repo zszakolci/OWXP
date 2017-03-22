cc9f2ee92956c72a9d55bed86d3f75e079ced7a3
NorbertKocsis <norbert.kocsis@liferay.com>
2017-03-22 12:49:38 +0100

    OWXP-129 Add custom MentionsMatcher for WikiPage - makes solution in hotfix obsolete

---
+++ modules/wiki-mentions-web/src/main/java/com/liferay/mentions/web/wiki/WikiMentionsMatcher.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mentions.web.wiki;
+
+import com.liferay.mentions.matcher.BaseRegularExpressionMentionsMatcher;
+import com.liferay.mentions.matcher.MentionsMatcher;
+import com.liferay.mentions.matcher.MentionsMatcherUtil;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Norbert Kocsis
+ *
+ */
+@Component(
+	immediate = true,
+	property = {
+		"model.class.name=com.liferay.wiki.model.WikiPage",
+		"service.ranking:Integer=" + Integer.MIN_VALUE
+	},
+	service = MentionsMatcher.class
+)
+public class WikiMentionsMatcher extends BaseRegularExpressionMentionsMatcher {
+
+	@Override
+	protected String getRegularExpression() {
+		return String.format(
+			_MENTIONS_REGULAR_EXPRESSION_TEMPLATE,
+			MentionsMatcherUtil.getScreenNameRegularExpression());
+	}
+
+	private static final String _MENTIONS_REGULAR_EXPRESSION_TEMPLATE =
+		"(?:\\s|^|\\]|>|\\|)(?:@|&#64;)(%s)(?=[<\\[\\s]|$)";
+
+}
