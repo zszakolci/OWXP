2a262e6d58047c9ec40c38b920c995d9214d546d
Tamas Molnar <tamas.molnar@liferay.com>
2017-03-22 17:40:39 -0700

    Revert "OWXP-110 Set higher service ranking"
    This reverts commit 1001ea2bdc8ab6997d875a768ba72dd9b8a06b33.

--- modules/notifications/src/main/java/com/liferay/notifications/web/internal/application/list/NotificationsPanelApp.java
+++ modules/notifications/src/main/java/com/liferay/notifications/web/internal/application/list/NotificationsPanelApp.java
@@ -21,7 +21,12 @@ import com.liferay.notifications.web.internal.constants.NotificationsPortletKeys
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserNotificationDeliveryConstants;
+import com.liferay.portal.kernel.service.PortalPreferencesLocalServiceUtil;
 import com.liferay.portal.kernel.service.UserNotificationEventLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PortletKeys;
+
+import javax.portlet.PortletPreferences;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -34,8 +39,7 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"javax.portlet.name=" + NotificationsPortletKeys.NOTIFICATIONS,
 		"panel.app.order:Integer=400",
-		"panel.category.key=" + PanelCategoryKeys.USER_MY_ACCOUNT,
-		"service.ranking:Integer=100"
+		"panel.category.key=" + PanelCategoryKeys.USER_MY_ACCOUNT
 	},
 	service = PanelApp.class
 )
@@ -47,10 +51,31 @@ public class NotificationsPanelApp extends BasePanelApp {
 			return 0;
 		}
 
-		return _userNotificationEventLocalService.
-			getArchivedUserNotificationEventsCount(
-				user.getUserId(),
-				UserNotificationDeliveryConstants.TYPE_WEBSITE, false);
+		PortletPreferences portletPreferences =
+			PortalPreferencesLocalServiceUtil.getPreferences(
+				user.getUserId(), PortletKeys.PREFS_OWNER_TYPE_USER);
+
+		boolean useLegacyUserNotifiationEventsCount = GetterUtil.getBoolean(
+			portletPreferences.getValue(
+				"useLegacyUserNotifiationEventsCount",
+				Boolean.TRUE.toString()));
+
+		int userNotifiationEventsCount = 0;
+
+		if (useLegacyUserNotifiationEventsCount) {
+			userNotifiationEventsCount =
+				_userNotificationEventLocalService.
+					getArchivedUserNotificationEventsCount(
+						user.getUserId(),
+						UserNotificationDeliveryConstants.TYPE_WEBSITE, false);
+		}
+		else {
+			userNotifiationEventsCount = GetterUtil.getInteger(
+				portletPreferences.getValue(
+					"userNotifiationEventsCount", null));
+		}
+
+		return userNotifiationEventsCount;
 	}
 
 	@Override
