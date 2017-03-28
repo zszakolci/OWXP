49a7fb7e7ba6205e3222762157025d5dd958e75b
rimi saadou <rimi.saadou@liferay.com>
2017-03-28 11:11:56 +0200

    OWXP-213 Fixed issue and RF micro-maintainance-portlet

--- modules/micro-maintainance-portlet/src/main/java/com/liferay/micro/maintainance/util/TaskHandlerUtil.java
+++ modules/micro-maintainance-portlet/src/main/java/com/liferay/micro/maintainance/util/TaskHandlerUtil.java
@@ -1,17 +1,39 @@
 package com.liferay.micro.maintainance.util;
 
+import com.liferay.micro.maintainance.api.Task;
+import com.liferay.micro.maintainance.api.TaskHandler;
+import com.liferay.portal.kernel.exception.PortalException;
+
 import java.util.List;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import com.liferay.micro.maintainance.api.Task;
-import com.liferay.micro.maintainance.api.TaskHandler;
-import com.liferay.portal.kernel.exception.PortalException;
-
+/**
+ * @author Rimi Saadou
+ * @author Laszlo Hudak
+ */
 @Component
 public class TaskHandlerUtil {
+
+	public static List<Task> getAvailableFlags(long wikiPageId)
+		throws PortalException {
+
+		return _taskHandler.getAvailableFlags(wikiPageId);
+	}
+
+	public static int getVote(long userId, long wikiPageId, long taskId)
+		throws PortalException {
+
+		return _taskHandler.getVote(userId, wikiPageId, taskId);
+	}
+
+	public static void vote(long userId, long wikiPageId, long taskId, int vote)
+		throws PortalException {
+
+		_taskHandler.vote(userId, wikiPageId, taskId, vote);
+	}
 
 	public Map<Long, Task> getTaskEntries() {
 		return _taskHandler.getTaskEntries();
@@ -21,28 +43,11 @@ public class TaskHandlerUtil {
 		return _taskHandler.getTaskEntryList();
 	}
 
-	public int getVote(long userId, long wikiPageId, long taskId)
-		throws PortalException {
-
-		return _taskHandler.getVote(userId, wikiPageId, taskId);
-	}
-
-	public void vote(long userId, long wikiPageId, long taskId, int vote) 
-		throws PortalException {
-
-		_taskHandler.vote(userId, wikiPageId, taskId, vote);
-	}
-
-	public static List<Task> getAvailableFlags(long wikiPageId) 
-		throws PortalException {
-
-		return _taskHandler.getAvailableFlags(wikiPageId);
-	}
-
 	@Reference(unbind = "-")
 	protected void setTaskHandler(TaskHandler taskHandler) {
 		_taskHandler = taskHandler;
 	}
 
 	private static TaskHandler _taskHandler;
+
 }
