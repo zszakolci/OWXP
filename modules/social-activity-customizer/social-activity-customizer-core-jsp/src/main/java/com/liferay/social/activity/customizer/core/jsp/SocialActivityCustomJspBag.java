46b1cd3c796150f2466bd714e8e203faf04f5fe8
istvansajtos <istvan.sajtos@liferay.com>
2017-07-19 15:10:09 +0200

    GROW-117 Add core JSP module

---
+++ modules/social-activity-customizer/social-activity-customizer-core-jsp/src/main/java/com/liferay/social/activity/customizer/core/jsp/SocialActivityCustomJspBag.java
@@ -0,0 +1,37 @@
+package com.liferay.social.activity.customizer.core.jsp;
+
+import com.liferay.portal.deploy.hot.CustomJspBag;
+import com.liferay.portal.kernel.url.URLContainer;
+
+import java.util.List;
+
+/**
+ * @author Istvan Sajtos
+ */
+public class SocialActivityCustomJspBag implements CustomJspBag {
+
+	@Override
+	public String getCustomJspDir() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getCustomJsps() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public URLContainer getURLContainer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isCustomJspGlobal() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
