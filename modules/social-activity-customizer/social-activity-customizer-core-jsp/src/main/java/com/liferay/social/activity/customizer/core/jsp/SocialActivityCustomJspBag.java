d92c13769f5349fd1ed203e9ff15205c4a57c032
istvansajtos <istvan.sajtos@liferay.com>
2017-07-19 17:35:08 +0200

    GROW-117 Implement custom JspBag

--- modules/social-activity-customizer/social-activity-customizer-core-jsp/src/main/java/com/liferay/social/activity/customizer/core/jsp/SocialActivityCustomJspBag.java
+++ modules/social-activity-customizer/social-activity-customizer-core-jsp/src/main/java/com/liferay/social/activity/customizer/core/jsp/SocialActivityCustomJspBag.java
@@ -3,35 +3,91 @@ package com.liferay.social.activity.customizer.core.jsp;
 import com.liferay.portal.deploy.hot.CustomJspBag;
 import com.liferay.portal.kernel.url.URLContainer;
 
+import java.net.URL;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Istvan Sajtos
  */
+@Component(
+	immediate = true,
+	property = {
+		"context.id=SocialActivityCustomJspBag",
+		"context.name=Social Activity Custom JSP Bag",
+		"service.ranking:Integer=100"
+	}
+)
 public class SocialActivityCustomJspBag implements CustomJspBag {
 
 	@Override
 	public String getCustomJspDir() {
-		// TODO Auto-generated method stub
-		return null;
+		return "META-INF/jsps/";
 	}
 
 	@Override
 	public List<String> getCustomJsps() {
-		// TODO Auto-generated method stub
-		return null;
+		return _customJsps;
 	}
 
 	@Override
 	public URLContainer getURLContainer() {
-		// TODO Auto-generated method stub
-		return null;
+		return _urlContainer;
 	}
 
 	@Override
 	public boolean isCustomJspGlobal() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+
+		_customJsps = new ArrayList<>();
+
+		Enumeration<URL> entries = _bundle.findEntries(
+			getCustomJspDir(), "*.jsp", true);
+
+		while (entries.hasMoreElements()) {
+			URL url = entries.nextElement();
+
+			_customJsps.add(url.getPath());
+		}
+	}
+
+	private Bundle _bundle;
+	private List<String> _customJsps;
+
+	private final URLContainer _urlContainer = new URLContainer() {
+
+		@Override
+		public URL getResource(String name) {
+			return _bundle.getEntry(name);
+		}
+
+		@Override
+		public Set<String> getResources(String path) {
+			Set<String> paths = new HashSet<>();
+
+			for (String entry : _customJsps) {
+				if (entry.startsWith(path)) {
+					paths.add(entry);
+				}
+			}
+
+			return paths;
+		}
+
+	};
 
 }
