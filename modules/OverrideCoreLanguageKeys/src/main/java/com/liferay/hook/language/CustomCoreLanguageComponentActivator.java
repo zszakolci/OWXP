b8b4f14e3390ddb5d52c994a7de199adbaa5c10d
Uğurcan Çetin <ugurcan.cetin@liferay.com>
2017-09-13 15:25:07 +0200

    GROW-351 Override core language keys for wiki activities on activity portlet

---
+++ modules/OverrideCoreLanguageKeys/src/main/java/com/liferay/hook/language/CustomCoreLanguageComponentActivator.java
@@ -0,0 +1,34 @@
+package com.liferay.hook.language;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.language.UTF8Control;
+
+
+/**
+ * @author Ugurcan Cetin
+ */
+
+@Component(
+	    property = { "language.id=en_US" }, 
+	    service = ResourceBundle.class
+	)
+public class CustomCoreLanguageComponentActivator extends ResourceBundle {
+
+	@Override
+    protected Object handleGetObject(String key) {
+        return _resourceBundle.getObject(key);
+    }
+
+    @Override
+    public Enumeration<String> getKeys() {
+        return _resourceBundle.getKeys();
+    }
+
+    private final ResourceBundle _resourceBundle = ResourceBundle.getBundle(
+        "content.Language", UTF8Control.INSTANCE);
+	
+}
