8aeb36996d9bb5de63460b3d2460a4dac3de06d5
Tamas Molnar <tamas.molnar@liferay.com>
2018-02-15 14:29:31 +0100

    GROW-488 - Apply LPS-75564

--- modules/wiki-mentions-web/src/main/java/com/liferay/mentions/web/editor/configuration/BaseMentionsEditorConfigContributor.java
+++ modules/wiki-mentions-web/src/main/java/com/liferay/mentions/web/editor/configuration/BaseMentionsEditorConfigContributor.java
@@ -1,21 +1,21 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ *
+ *
  */
 
 package com.liferay.mentions.web.editor.configuration;
 
+import com.liferay.mentions.constants.MentionsPortletKeys;
 import com.liferay.mentions.matcher.MentionsMatcherUtil;
-import com.liferay.mentions.web.constants.MentionsPortletKeys;
 import com.liferay.portal.kernel.editor.configuration.BaseEditorConfigContributor;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
@@ -31,7 +31,7 @@ import java.util.Map;
 import javax.portlet.PortletURL;
 
 /**
- * @author Sergio González
+ * @author Sergio Gonz??lez
  */
 public class BaseMentionsEditorConfigContributor
 	extends BaseEditorConfigContributor {
