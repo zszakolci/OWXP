67889812d3439e74ca47cf5d641726a8a26e29a5
Tamas Molnar <tamas.molnar@liferay.com>
2018-03-13 21:23:03 +0100

    GROW-503 Create initial service module: blade create -t service -p com.liferay.grow.WikiHelper -c WikiContributorHelper -s WikiContributor WikiHelper

---
+++ modules/WikiHelper/src/main/java/com/liferay/grow/WikiHelper/WikiContributorHelper.java
@@ -0,0 +1,21 @@
+package com.liferay.grow.WikiHelper;
+
+import WikiContributor;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Liferay
+ */
+@Component(
+	immediate = true,
+	property = {
+		// TODO enter required service properties
+	},
+	service = WikiContributor.class
+)
+public class WikiContributorHelper implements WikiContributor {
+
+	// TODO enter required service methods
+
+}
