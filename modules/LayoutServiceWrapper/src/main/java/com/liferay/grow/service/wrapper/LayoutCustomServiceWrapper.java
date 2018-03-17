b8a6b48c358279ab3a547e11e2d3520657f26506
Tamas Molnar <tamas.molnar@liferay.com>
2018-03-17 02:58:47 +0100

    GROW-506 Withdraw the View permission from the Guest role for the default user Profile Page

--- modules/LayoutServiceWrapper/src/main/java/com/liferay/grow/service/wrapper/LayoutCustomServiceWrapper.java
+++ modules/LayoutServiceWrapper/src/main/java/com/liferay/grow/service/wrapper/LayoutCustomServiceWrapper.java
@@ -1,24 +1,118 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.grow.service.wrapper;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.RoleConstants;
 import com.liferay.portal.kernel.service.LayoutLocalServiceWrapper;
-
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.util.PropsValues;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Liferay
+ * @author Tamas Molnar
  */
-@Component(
-	immediate = true,
-	property = {
-	},
-	service = ServiceWrapper.class
-)
+@Component(immediate = true, property = {}, service = ServiceWrapper.class)
 public class LayoutCustomServiceWrapper extends LayoutLocalServiceWrapper {
 
 	public LayoutCustomServiceWrapper() {
 		super(null);
 	}
+
+	@Override
+	public Layout addLayout(
+			long userId, long groupId, boolean privateLayout,
+			long parentLayoutId, String name, String title, String description,
+			String type, boolean hidden, String friendlyURL,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		Layout layout = super.addLayout(
+			userId, groupId, privateLayout, parentLayoutId, name, title,
+			description, type, hidden, friendlyURL, serviceContext);
+
+		try {
+			Group group = layout.getGroup();
+
+			String layoutFriendlyURL = layout.getFriendlyURL();
+
+			String defaultUserPublicLayoutFriendlyURL = getFriendlyURL(
+				PropsValues.DEFAULT_USER_PUBLIC_LAYOUT_FRIENDLY_URL);
+
+			// Remove the Guest role's View permission for default user Profile
+			// Page
+
+			if (group.isUser() && layout.isPublicLayout() &&
+				layoutFriendlyURL.equals(defaultUserPublicLayoutFriendlyURL)) {
+
+				Role guestRole = _roleLocalService.getRole(
+					layout.getCompanyId(), RoleConstants.GUEST);
+
+				_resourcePermissionLocalService.setResourcePermissions(
+					layout.getCompanyId(), Layout.class.getName(),
+					ResourceConstants.SCOPE_INDIVIDUAL,
+					String.valueOf(layout.getPlid()), guestRole.getRoleId(),
+					new String[0]);
+			}
+		}
+		catch (Exception e) {
+			_log.error(
+				"Cannot withdraw View permission for Layout with plid " +
+					layout.getPlid(),
+				e);
+		}
+
+		return layout;
+	}
+
+	protected String getFriendlyURL(String friendlyURL) {
+		friendlyURL = GetterUtil.getString(friendlyURL);
+
+		return FriendlyURLNormalizerUtil.normalize(friendlyURL);
+	}
+
+	@Reference(unbind = "-")
+	protected void setResourcePermissionLocalService(
+		ResourcePermissionLocalService resourcePermissionLocalService) {
+
+		_resourcePermissionLocalService = resourcePermissionLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setRoleLocalService(RoleLocalService roleLocalService) {
+		_roleLocalService = roleLocalService;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutCustomServiceWrapper.class);
+
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+	private RoleLocalService _roleLocalService;
 
 }
