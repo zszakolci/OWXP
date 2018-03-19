bb0ea8e3791cec644149f40e733b85168a434a7b
Dávid Balatoni <balcsida@gmail.com>
2018-03-19 15:07:55 +0100

    Subscriber Portlet shouldn't be instanceable

--- modules/owxp-web/src/main/java/com/liferay/owxp/subscribe/portlet/OWXPSubscribePortlet.java
+++ modules/owxp-web/src/main/java/com/liferay/owxp/subscribe/portlet/OWXPSubscribePortlet.java
@@ -35,7 +35,7 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true,
 	property = {
 		"com.liferay.portlet.display-category=category.social",
-		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.instanceable=false",
 		"javax.portlet.display-name=GROW subscriber",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
