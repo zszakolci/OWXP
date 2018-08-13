43d898dbe34ed72891b786259ccc7fb946c2f16c
Tamas Molnar <tamas.molnar@liferay.com>
2018-08-13 14:43:20 +0200

    GROW-514 Readd mentioning - see GROW-7

--- modules/WikiPageServiceOverride/src/main/java/com/liferay/wiki/internal/service/WikiPageCustomServiceWrapper.java
+++ modules/WikiPageServiceOverride/src/main/java/com/liferay/wiki/internal/service/WikiPageCustomServiceWrapper.java
@@ -14,13 +14,23 @@
 
 package com.liferay.wiki.internal.service;
 
+import java.io.Serializable;
+import java.util.Map;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import com.liferay.exportimport.kernel.lar.ExportImportThreadLocal;
+import com.liferay.mentions.configuration.MentionsGroupServiceConfiguration;
+import com.liferay.mentions.util.MentionsNotifier;
+import com.liferay.mentions.util.MentionsUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.wiki.model.WikiPage;
 import com.liferay.wiki.service.WikiPageLocalService;
 import com.liferay.wiki.service.WikiPageLocalServiceWrapper;
@@ -78,12 +88,72 @@ public class WikiPageCustomServiceWrapper extends WikiPageLocalServiceWrapper {
 		return page;
 	}
 
+	@Override
+	public WikiPage updateStatus(
+			long userId, WikiPage page, int status,
+			ServiceContext serviceContext,
+			Map<String, Serializable> workflowContext)
+		throws PortalException {
+
+		int oldStatus = page.getStatus();
+
+		page = super.updateStatus(
+			userId, page, status, serviceContext, workflowContext);
+
+		if ((status != WorkflowConstants.STATUS_APPROVED) ||
+				(oldStatus == WorkflowConstants.STATUS_IN_TRASH)) {
+
+			return page;
+		}
+
+		long siteGroupId = PortalUtil.getSiteGroupId(page.getGroupId());
+
+		if (!MentionsUtil.isMentionsEnabled(siteGroupId)) {
+			return page;
+		}
+
+		String contentURL = (String)serviceContext.getAttribute("contentURL");
+
+		if (Validator.isNull(contentURL)) {
+			serviceContext.setAttribute(
+				"contentURL", workflowContext.get("url"));
+		}
+
+		MentionsGroupServiceConfiguration mentionsGroupServiceConfiguration =
+			_configurationProvider.getCompanyConfiguration(
+				MentionsGroupServiceConfiguration.class, page.getCompanyId());
+
+		_mentionsNotifier.notify(
+			userId, page.getGroupId(), page.getTitle(), page.getContent(),
+			WikiPage.class.getName(), page.getPageId(),
+			mentionsGroupServiceConfiguration.assetEntryMentionEmailSubject(),
+			mentionsGroupServiceConfiguration.assetEntryMentionEmailBody(),
+			serviceContext);
+
+		return page;
+	}
+
+	@Reference(unbind = "-")
+	protected void setConfigurationProvider(
+		ConfigurationProvider configurationProvider) {
+
+		_configurationProvider = configurationProvider;
+	}
+
+	@Reference(unbind = "-")
+	protected void setMentionsNotifier(MentionsNotifier mentionsNotifier) {
+		_mentionsNotifier = mentionsNotifier;
+	}
+
 	@Reference(unbind = "-")
 	protected void setWikiPageLocalService(
 		WikiPageLocalService wikiPageLocalService) {
 
 		_wikiPageLocalService = wikiPageLocalService;
 	}
+
+	private ConfigurationProvider _configurationProvider;
+	private MentionsNotifier _mentionsNotifier;
 
 	private WikiPageLocalService _wikiPageLocalService;
 	
