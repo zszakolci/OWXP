2ed8868d4108e3ee23cf26876d91af5d5736ab4e
Tamas Molnar <tamas.molnar@liferay.com>
2018-09-11 14:46:15 +0200

    GROW-514 Upgrade MarkdownEngine

--- modules/wiki-engine-markdown/src/main/java/com/liferay/wiki/engine/markdown/MarkdownEngine.java
+++ modules/wiki-engine-markdown/src/main/java/com/liferay/wiki/engine/markdown/MarkdownEngine.java
@@ -14,25 +14,12 @@
 
 package com.liferay.wiki.engine.markdown;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.portlet.FriendlyURLMapper;
-import com.liferay.portal.kernel.portlet.Router;
-import com.liferay.portal.kernel.util.AggregateResourceBundleLoader;
-import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.ResourceBundleLoader;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.language.LanguageResources;
-import com.liferay.wiki.configuration.WikiGroupServiceConfiguration;
-import com.liferay.wiki.constants.WikiPortletKeys;
+import com.liferay.wiki.engine.BaseWikiEngine;
 import com.liferay.wiki.engine.WikiEngine;
-import com.liferay.wiki.engine.input.editor.common.BaseInputEditorWikiEngine;
 import com.liferay.wiki.exception.PageContentException;
 import com.liferay.wiki.model.WikiPage;
-import com.liferay.wiki.service.WikiNodeLocalService;
 
 import com.vladsch.flexmark.ast.Node;
 import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
@@ -50,18 +37,13 @@ import com.vladsch.flexmark.parser.Parser.Builder;
 import com.vladsch.flexmark.util.options.MutableDataSet;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.portlet.PortletURL;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-
-import net.htmlparser.jericho.Source;
-import net.htmlparser.jericho.StartTag;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -70,9 +52,10 @@ import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Norbert Kocsis
+ * @author Tamas Molnar
  */
-@Component(service = WikiEngine.class)
-public class MarkdownEngine extends BaseInputEditorWikiEngine {
+@Component(immediate = true, service = WikiEngine.class)
+public class MarkdownEngine extends BaseWikiEngine {
 
 	@Override
 	public String convert(
@@ -105,77 +88,14 @@ public class MarkdownEngine extends BaseInputEditorWikiEngine {
 
 	@Override
 	public String getHelpURL() {
-		return "https://daringfireball.net/projects/markdown/syntax";
+		return "https://github.github.com/gfm/";
 	}
 
 	@Override
 	public Map<String, Boolean> getOutgoingLinks(WikiPage page)
 		throws PageContentException {
 
-		String content = convert(page, null, null, null);
-
-		if (Validator.isNull(content)) {
-			return Collections.emptyMap();
-		}
-
-		Map<String, Boolean> links = new HashMap<>();
-
-		Source source = new Source(content);
-
-		List<StartTag> startTags = source.getAllStartTags("a");
-
-		for (StartTag startTag : startTags) {
-			String href = startTag.getAttributeValue("href");
-
-			if (Validator.isNull(href)) {
-				continue;
-			}
-
-			int pos = href.lastIndexOf(_friendlyURLMapping);
-
-			if (pos == -1) {
-				continue;
-			}
-
-			String friendlyURL = href.substring(
-				pos + _friendlyURLMapping.length());
-
-			if (friendlyURL.endsWith(StringPool.SLASH)) {
-				friendlyURL = friendlyURL.substring(
-					0, friendlyURL.length() - 1);
-			}
-
-			Map<String, String> routeParameters = new HashMap<>();
-
-			if (!_router.urlToParameters(friendlyURL, routeParameters)) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"No route could be found to match URL " + friendlyURL);
-				}
-
-				continue;
-			}
-
-			String title = routeParameters.get("title");
-			String nodeName = routeParameters.get("nodeName");
-
-			if (Validator.isNull(title) || Validator.isNull(nodeName)) {
-				continue;
-			}
-
-			try {
-				_wikiNodeLocalService.getNode(page.getGroupId(), nodeName);
-
-				links.put(StringUtil.toLowerCase(title), Boolean.TRUE);
-			}
-			catch (PortalException pe) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(pe.getMessage());
-				}
-			}
-		}
-
-		return links;
+		return new HashMap<>();
 	}
 
 	@Override
@@ -221,46 +141,21 @@ public class MarkdownEngine extends BaseInputEditorWikiEngine {
 	}
 
 	@Override
+	protected ServletContext getEditPageServletContext() {
+		return _servletContext;
+	}
+
+	@Override
 	protected ServletContext getHelpPageServletContext() {
 		return null;
 	}
 
-	@Override
-	protected ResourceBundleLoader getResourceBundleLoader() {
-		return _resourceBundleLoader;
-	}
-
 	@Reference(
-		target = "(javax.portlet.name=" + WikiPortletKeys.WIKI + ")",
+		target = "(osgi.web.symbolicname=com.liferay.wiki.engine.text)",
 		unbind = "-"
 	)
-	protected void setFriendlyURLMapper(FriendlyURLMapper friendlyURLMapper) {
-		_friendlyURLMapping =
-			Portal.FRIENDLY_URL_SEPARATOR + friendlyURLMapper.getMapping();
-
-		_router = friendlyURLMapper.getRouter();
-	}
-
-	@Reference(target = "(bundle.symbolic.name=owxp.lang)", unbind = "-")
-	protected void setResourceBundleLoader(
-		ResourceBundleLoader resourceBundleLoader) {
-
-		_resourceBundleLoader = new AggregateResourceBundleLoader(
-			resourceBundleLoader, LanguageResources.RESOURCE_BUNDLE_LOADER);
-	}
-
-	@Reference
-	protected void setWikiGroupServiceConfiguration(
-		WikiGroupServiceConfiguration wikiGroupServiceConfiguration) {
-
-		_wikiGroupServiceConfiguration = wikiGroupServiceConfiguration;
-	}
-
-	@Reference(unbind = "-")
-	protected void setWikiNodeLocalService(
-		WikiNodeLocalService wikiNodeLocalService) {
-
-		_wikiNodeLocalService = wikiNodeLocalService;
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
 	}
 
 	private MutableDataSet _getOptions() {
@@ -293,12 +188,8 @@ public class MarkdownEngine extends BaseInputEditorWikiEngine {
 
 	private static final Log _log = LogFactoryUtil.getLog(MarkdownEngine.class);
 
-	private String _friendlyURLMapping;
 	private ThreadLocal<Parser> _parserThreadLocal;
 	private ThreadLocal<HtmlRenderer> _rendererThreadLocal;
-	private ResourceBundleLoader _resourceBundleLoader;
-	private Router _router;
-	private WikiGroupServiceConfiguration _wikiGroupServiceConfiguration;
-	private WikiNodeLocalService _wikiNodeLocalService;
+	private ServletContext _servletContext;
 
 }
