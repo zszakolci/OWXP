fb3067ef31e67225b7ad6781ab063fe85846bebc
Vilmos Papp <vilmos.papp@liferay.com>
2018-11-10 16:32:32 +0100

    GROW-618 Fixed Nofitification handling and added e-mail sending for user who received the badge

--- modules/Badges/Badges-service/src/main/java/com/liferay/grow/gamification/service/impl/BadgeLocalServiceImpl.java
+++ modules/Badges/Badges-service/src/main/java/com/liferay/grow/gamification/service/impl/BadgeLocalServiceImpl.java
@@ -15,26 +15,34 @@
 package com.liferay.grow.gamification.service.impl;
 
 
+import com.liferay.document.library.kernel.service.DLAppLocalServiceUtil;
+import com.liferay.document.library.kernel.util.DLUtil;
 import com.liferay.grow.gamification.badges.notification.BadgeReceivedSubscritpionSender;
 import com.liferay.grow.gamification.badges.notification.constants.BadgeNotificationPortletKeys;
 import com.liferay.grow.gamification.badges.notification.portlet.BadgeNotificationPortlet;
 import com.liferay.grow.gamification.model.Badge;
+import com.liferay.grow.gamification.model.BadgeType;
 import com.liferay.grow.gamification.service.base.BadgeLocalServiceBaseImpl;
+import com.liferay.mail.kernel.model.MailMessage;
+import com.liferay.mail.kernel.service.MailServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserNotificationDeliveryConstants;
+import com.liferay.portal.kernel.model.UserNotificationEvent;
 import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.service.UserNotificationEventLocalService;
 import com.liferay.portal.kernel.service.UserNotificationEventLocalServiceUtil;
 import com.liferay.portal.kernel.settings.LocalizedValuesMap;
+import com.liferay.portal.kernel.util.StringUtil;
 
-import java.util.Date;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Locale;
 
-import org.osgi.service.component.annotations.Reference;
+import javax.mail.internet.InternetAddress;
 
 /**
  * The implementation of the badge local service.
@@ -95,7 +103,7 @@ public class BadgeLocalServiceImpl extends BadgeLocalServiceBaseImpl {
 		subscriptionSender.setClassName(BadgeNotificationPortlet.class.getName());
 		subscriptionSender.setCompanyId(badge.getCompanyId());
 
-		subscriptionSender.setCurrentUserId(badge.getUserId());
+		subscriptionSender.setCurrentUserId(badge.getToUserId());
 		subscriptionSender.setEntryTitle(entryTitle);
 		subscriptionSender.setFrom(fromUser.getEmailAddress(), fromUser.getFullName());
 		subscriptionSender.setHtmlFormat(true);
@@ -120,18 +128,80 @@ public class BadgeLocalServiceImpl extends BadgeLocalServiceBaseImpl {
 
 		JSONObject payloadJSON = JSONFactoryUtil.createJSONObject();
 		payloadJSON.put(BadgeNotificationPortletKeys.BADGE_TYPE, badgeType);
+		payloadJSON.put(BadgeNotificationPortletKeys.BADGE_COMMENT, badge.getDescription());
+
+		UserNotificationEvent userNotificationEvent =
+			UserNotificationEventLocalServiceUtil.createUserNotificationEvent(
+				counterLocalService.increment());
+
+		userNotificationEvent.setCompanyId(badge.getCompanyId());
+		userNotificationEvent.setDeliverBy(0);
+		userNotificationEvent.setDelivered(true);
+		userNotificationEvent.setDeliveryType(UserNotificationDeliveryConstants.TYPE_WEBSITE);
+		userNotificationEvent.setTimestamp(System.currentTimeMillis());
+		userNotificationEvent.setPayload(payloadJSON.toString());
+		userNotificationEvent.setType(BadgeNotificationPortletKeys.BadgeNotification);
+		userNotificationEvent.setUserId(badge.getToUserId());
+
+		UserNotificationEventLocalServiceUtil.addUserNotificationEvent(userNotificationEvent);
+
+		InternetAddress recipient = null;
+		InternetAddress sender = null;
 
 		try {
-			UserNotificationEventLocalServiceUtil.addUserNotificationEvent(badge.getToUserId(), 
-					BadgeNotificationPortletKeys.BadgeNotification, 
-					(new Date()).getTime(),
-					0,
-					payloadJSON.toString(),
-					false, new ServiceContext());
-		} catch (PortalException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
+			MailMessage mailMessage = _getMailMessage(badge);
+
+			User user = userLocalService.getUserById(badge.getToUserId());
+
+			sender = new InternetAddress(_BADGE_EMAIL_SENDER_ADDRESS, _BADGE_EMAIL_SENDER_PERSONAL);
+			recipient = new InternetAddress(user.getEmailAddress(), user.getFullName());
+
+			mailMessage.setFrom(sender);
+			mailMessage.setHTMLFormat(true);
+			mailMessage.setSubject(_BADGE_EMAIL_SUBJECT);
+
+			mailMessage.setTo(recipient);
+
+
+			MailServiceUtil.sendEmail(mailMessage);
+		}
+		catch (UnsupportedEncodingException uee) {
+			uee.printStackTrace();
+		}
+		catch (PortalException pe) {
+			pe.printStackTrace();
+		}
 	}
 
+	private MailMessage _getMailMessage(Badge badge) throws PortalException {
+
+		BadgeType badgeType = badgeTypeLocalService.getBadgeType(badge.getBadgeTypeId());
+		MailMessage mailMessage = new MailMessage();
+
+		String content = _BADGE_EMAIL_BODY;
+
+		FileEntry fileEntry = DLAppLocalServiceUtil.getFileEntry(badgeType.getFileEntryId());
+		String downloadUrl = DLUtil.getPreviewURL(fileEntry, fileEntry.getFileVersion(), null, "", false, true);
+
+		content = StringUtil.replace(content, "${badgeType}", badgeType.getType());
+		content = StringUtil.replace(content, "${bagdeImageLink}", downloadUrl);
+		content = StringUtil.replace(content, "${colleague}", badge.getUserName());
+
+		mailMessage.setBody(content);
+
+		return mailMessage;
+	}
+
+	private static final String _BADGE_EMAIL_SENDER_ADDRESS = "admin@liferay,com";
+	private static final String _BADGE_EMAIL_SENDER_PERSONAL = "GROW Badge Notification";
+	private static final String _BADGE_EMAIL_SUBJECT = "You received a Bagde!";
+	private static final String _BADGE_EMAIL_BODY = "<html><head><meta http-equiv=\"content-type\" " +
+			"content=\"text/html; charset=UTF-8\"><title>You received a Badge!</title></head>" +
+			"<body alink=\"#EE0000\" link=\"#0000EE\" vlink=\"#551A8B\" text=\"#000000\" "+
+			"bgcolor=\"#eeeeee\"><center><table style=\"border-radius: 10px;\" bgcolor=\"#ffffff\" " +
+			"width=\"90%\" cellspacing=\"2\" cellpadding=\"2\" border=\"0\"><tbody><tr align=\"center\">" +
+			"<td valign=\"top\">Good news! You've just received a ${badgeType} from ${colleague}!</td>" +
+			"</tr><tr align=\"center\"><td valign=\"top\"><img src=\"${bagdeImageLink}\"" +
+			"alt=\"Bagde image\" height=\"300\" width=\"300\"></td></tr><tr align=\"center\">" +
+			"<td valign=\"top\">Congratulations!</td></tr></tbody></table></center></body></html>";
 }
